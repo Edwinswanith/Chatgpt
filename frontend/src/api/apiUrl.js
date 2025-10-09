6059d01ef61d43a85fb089d943acd1c8e227a621
bizzzup <bizzzup2022@outlook.com>
2025-10-09 16:21:58 +0530

    Chnage Url

--- frontend/src/api/apiUrl.js
+++ frontend/src/api/apiUrl.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://6h82fbwn-5000.inc1.devtunnels.ms/',
+    baseURL: 'http://127.0.0.1:5000',
     withCredentials: true,
 });
 
